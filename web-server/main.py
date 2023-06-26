39693b6250cca40ee324bced0312e6a3dace6b14
Juan Ferraro <juansebastianferraro@gmail.com>
2023-06-26 10:05:57 -0500

    FastAPI 📚

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/') #A esto se le llama decorador
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class = HTMLResponse) 
+def contact():
+    return """
+        <h1>Hola, acá aprendiendo python</h1>
+        <p>Parrafo parrafo parrafo</p>
+    """
 
 def run():
     store.get_categories()
